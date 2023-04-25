a1d1b821973de8c0a0aabcf62c94cfae812dcbbd
me19d506 <87779141+me19d506@users.noreply.github.com>
2023-04-25 10:30:01 +0530

    Add files via upload

---
+++ lhs.m
@@ -0,0 +1,14 @@
+% Code written by PV Divakar Raju and Neeraj mishra, IIT Tirupati May 2022
+
+% function is for latin hypercube sampling from uniform distribution.
+% reqires min,max and number of samples as input
+function f=lhs(min,max,nsample)
+nv      =length(min);                               %number of variables
+f       =zeros(nsample,nv); 
+random  =rand(nsample,nv);                          %random numbers 
+            
+for k=1: nv
+   indx     = randperm(nsample);                    %random permutation of the integers from 1 to n without repeating elements.
+   P        = (indx'-random(:,k))/nsample;
+   f(:,k)   = min(k) + P.* (max(k)-min(k));
+end
