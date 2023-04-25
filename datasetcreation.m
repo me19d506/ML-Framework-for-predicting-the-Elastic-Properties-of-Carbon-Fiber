cfce840c0b07d4096cee27d756b7f6bd7611fec5
me19d506 <87779141+me19d506@users.noreply.github.com>
2023-04-25 10:00:02 +0530

    Update datasetcreation.m

--- datasetcreation.m
+++ datasetcreation.m
@@ -14,20 +14,20 @@ n=1000; % no. of dataset
 
 % Elastic Properties of the fiber - assumed to be transversely isotropic 
 % Make sure lhsu.m should be along with this code to call the function and set the attribute values.
-E1f     = lhsu(160,600,n); % in GPA
-E2f     = lhsu(8,30,n); % in GPA
-nu12f   = lhsu(0.2,0.4,n);
-G12f    =  lhsu(2,40,n);% in GPA
-nu23f   = lhsu(0.05,0.5,n);
+E1f     = lhs(160,600,n); % in GPA
+E2f     = lhs(8,30,n); % in GPA
+nu12f   = lhs(0.2,0.4,n);
+G12f    =  lhs(2,40,n);% in GPA
+nu23f   = lhs(0.05,0.5,n);
 G23f    = (E2f./2.*(1+nu23f));% in GPA
 
 % Elastic Properties of the matrix - assumed to be isotropic
-Em      = lhsu(0.5,20,n);% in GPA
-num     = lhsu(0.3,0.49,n);
+Em      = lhs(0.5,20,n);% in GPA
+num     = lhs(0.3,0.49,n);
 Gm      = (Em./(2.*(1+num)));
 
 % Volume fraction of the composite
-Vf      = lhsu(0.3,0.7,n);
+Vf      = lhs(0.3,0.7,n);
  
 % Generate the samples
 INPUT_p = [E1f E2f nu12f G12f G23f nu23f Em num Gm Vf];
